072b70553a2941e614af848e83eef27fba3ea3ec
KatiaHernandez08 <127240669+KatiaHernandez08@users.noreply.github.com>
2023-03-08 18:52:53 -0600

    klhc:redecionar Home

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -8,6 +8,12 @@ class loginController
     public function ingresar(){
         if(isset($_POST["usuario"])&& isset($_POST["contrasena"])){
            $resp=$this->loginModel->validar($_POST);
+           if(isset($resp["id_usuario"])){
+            session_start();
+            $_SESSION=$resp;
+            $_SESSION["Hola"]="Bienvenida";
+            header("Location:../home/dashboard");
+           }
         }
         require_once "views/templates/header.php";
         require_once "views/login.php";
